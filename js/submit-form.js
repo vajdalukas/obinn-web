7af1d13036cdb17fd4c02a69f7889c59ef78e73e
Lukáš Vajda <lukasvajda@icloud.com>
2025-01-24 18:57:03 +0100

    working

---
+++ js/submit-form.js
@@ -0,0 +1,43 @@
+$(function () {
+  'use strict';
+
+  // Ambil semua formulir yang ingin kita terapkan gaya validasi kustom Bootstrap
+  const forms = $('.needs-validation');
+
+  // Loop melalui formulir dan mencegah pengiriman
+  forms.on('submit', function (event) {
+      const form = $(this);
+
+      if (!form[0].checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+      } else {
+          event.preventDefault();
+          $('.submit_form').html('Odesílá se...');
+          const toast = new bootstrap.Toast($('.success_msg')[0]);
+          const errtoast = new bootstrap.Toast($('.error_msg')[0]);
+
+          $.ajax({
+            "url": "https://sendmail.obinn.cz/",
+            "method": "POST",
+            "timeout": 0,
+            "processData": false,
+            "mimeType": "multipart/form-data",
+            "contentType": false,
+            "data": form,
+
+            success: function (response) {
+              if (response == 'success') {
+                  toast.show()
+                  $('.submit_form').html('Submit');
+              } else {
+                  errtoast.show()
+                  $('.submit_form').html('Submit');
+              }
+            },
+          });
+      }
+
+      form.addClass('was-validated');
+  });
+});
